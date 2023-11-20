73b9e7e7c2d61eea8c4da2fe04ac81af40542bca
liviabion <llb@cin.ufpe.br>
2023-11-20 20:17:01 -0300

    vector and operations

--- project.cpp
+++ project.cpp
@@ -49,11 +49,74 @@ double produtoEscalar(Ponto p1, Ponto p2) {
     return p1.x*p2.x + p1.y*p2.y + p1.z*p2.z;
 }
 
+
+struct Vector {
+    double x, y, z;
+    // Construtor padrão
+    Vector() : x(0.0), y(0.0), z(0.0) {}
+    // Construtor com parâmetros
+    Vector(double xCoord, double yCoord, double zCoord) : x(xCoord), y(yCoord), z(zCoord) {}
+};
+
+// Definição de operações sobre vetores:
+
+//Soma de vetores
+Vector somaVetores(Vector v1, Vector v2) {
+    Vector v;
+    v.x = v1.x + v2.x;
+    v.y = v1.y + v2.y;
+    v.z = v1.z + v2.z;
+    return v;
+}
+
+//Subtração de vetores
+Vector subtraiVetores(Vector v1, Vector v2) {
+    Vector v;
+    v.x = v1.x - v2.x;
+    v.y = v1.y - v2.y;
+    v.z = v1.z - v2.z;
+    return v;
+}
+
+//Produto escalar de vetores
+double produtoEscalar(Vector v1, Vector v2) {
+    return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
+}
+
+//Produto vetorial de vetores
+Vector produtoVetorial(Vector v1, Vector v2) {
+    Vector v;
+    v.x = v1.y * v2.z - v1.z * v2.y;
+    v.y = v1.z * v2.x - v1.x * v2.z;
+    v.z = v1.x * v2.y - v1.y * v2.x;
+    return v;
+}
+
+//Multiplicação de vetores por escalar
+Vector multiplicaVetorPorEscalar(Vector v1, double escalar) {
+    Vector v;
+    v.x = v1.x * escalar;
+    v.y = v1.y * escalar;
+    v.z = v1.z * escalar;
+    return v;
+}
+
+//Norma do vetor
+double normaVetor(Vector v) {
+    return sqrt(v.x*v.x + v.y*v.y + v.z*v.z);
+}
+
+
+//Impressão do vetor
+void imprimeVetor(Vector v) {
+    cout << "(" << v.x << ", " << v.y << ", " << v.z << ")" << endl;
+}
+
+
+
+
 // Função principal
 int main() {
-    Ponto p1 = {1.0, 2.0, 3.0}; // Inicialização direta usando chaves
-    Ponto p2(4.0, 5.0, 6.0);    // Inicialização por construção usando parênteses
-    // adicionar p1 e p2
-    Ponto p3 = somaPontos(p1, p2);
+
     return 0;
 }
